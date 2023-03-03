fc09045c92dd26f66fd096ace232da52e14d26aa
wancheng1990 <25022549@qq.com>
2023-03-03 09:45:49 +0800

    add demo

---
+++ audio/demo-2/player/pcm-player.js
@@ -0,0 +1,76 @@
+class PcmPlayer {
+    constructor(config) {
+        this.config = !!config ? config : {
+            bufferSize: 512,
+        };
+        this.audioContext = new (window.AudioContext || window.webkitAudioContext)({
+            sampleRate: 48000
+        });
+        this.scriptNode = null;
+        this.gainNode = null;
+        this.bufferQueue = new Float32Array(0);
+        this.silence = new Float32Array(this.config.bufferSize);
+    }
+
+    destroy() {
+        if (this.scriptNode) {
+            this.scriptNode.disconnect();
+            this.scriptNode = null;
+        }
+
+        if (this.gainNode) {
+            this.gainNode.disconnect();
+            this.gainNode = null;
+        }
+        this.audioContext = null;
+        this.bufferQueue = null;
+    }
+
+    play() {
+        this.scriptNode = this.audioContext.createScriptProcessor(this.config.bufferSize, 1, 1);
+        this.gainNode = this.audioContext.createGain();
+        this.scriptNode.connect(this.gainNode);
+        this.gainNode.connect(this.audioContext.destination);
+        this.gainNode.gain.value = 1;
+        this.scriptNode.onaudioprocess = (e) => {
+            if (this._getBufferEnough()) {
+                e.outputBuffer.getChannelData(0).set(this._read(this.config.bufferSize));
+            } else {
+                e.outputBuffer.getChannelData(0).set(this.silence);
+            }
+        }
+    }
+
+    pushBuffer(buffer) {
+        const len = buffer.length + this.bufferQueue.length;
+        const newBuffer = new Float32Array(len);
+        newBuffer.set(this.bufferQueue, 0);
+        newBuffer.set(buffer, this.bufferQueue.length);
+        this.bufferQueue = newBuffer;
+    }
+
+    mute() {
+        this.gainNode.gain.value = 0;
+    }
+
+    unMute() {
+        this.gainNode.gain.value = 1;
+    }
+
+
+    _read(len) {
+        const outBuffer = this.bufferQueue.subarray(0, len);
+        const inBuffer = this.bufferQueue.subarray(len, this.bufferQueue.length);
+        this.bufferQueue = inBuffer;
+        return outBuffer;
+    }
+
+    _getBufferEnough() {
+        return this._bufferLength() > this.config.bufferSize;
+    }
+
+    _bufferLength() {
+        return this.bufferQueue.length;
+    }
+
+}
---
+++ audio/demo-2/server/index.js
@@ -0,0 +1,41 @@
+
+var app = require('http').createServer(handler)
+var io = require('socket.io')(app);
+var fs = require('fs');
+var path = require('path');
+
+app.listen(8000, function() {
+    console.log('server start success, port:8085');
+});
+
+function handler (req, res) {
+    const reqPath = req.url;
+    const recorderPlayerPath = path.resolve(__dirname, '../');
+    fs.readFile(recorderPlayerPath + reqPath,
+        function (err, data) {
+            console.log('path:', recorderPlayerPath + reqPath);
+            if (err) {
+                res.writeHead(500);
+                return res.end('Error loading index.html');
+            }
+
+            res.writeHead(200);
+            res.end(data);
+        });
+}
+
+io.on('connection', function (socket) {
+    var socketId = socket.id;
+    console.log(socketId);
+    socket.on('pcm-data', function (data) {
+        // console.log('server receive data:', data);
+        // io.on('connection',function(socket){});//建立连接
+        // io.sockets.emit(约定参数，data)；//向全体人员广播
+        // io.emit(约定参数, data);//向全体人员广播
+        // socket.emit(约定参数，data)//发送信息
+        // socket.on(约定参数，callback）；//接收信息
+        // socket.on('disconnect',callback);//用户断开连接触发事件
+        console.log(data.pcm.length);
+        socket.broadcast.emit('to-player', data.pcm);
+    });
+});
